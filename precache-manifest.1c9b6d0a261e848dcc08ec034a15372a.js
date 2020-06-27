a10096fb5eef0450eeabd0b77280eba97694e504
theovast <theovast@MacBook-Pro-de-theovast-2.local>
2020-06-27 23:44:05 +0200

    Updates

---
+++ precache-manifest.1c9b6d0a261e848dcc08ec034a15372a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77813e8d520560042ef254e028d062f1",
+    "url": "/elihpoeht-react/index.html"
+  },
+  {
+    "revision": "7402e6d107b21648db8e",
+    "url": "/elihpoeht-react/static/css/main.eae731aa.chunk.css"
+  },
+  {
+    "revision": "f0f23b1eee1b0340a009",
+    "url": "/elihpoeht-react/static/js/2.4929d2c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elihpoeht-react/static/js/2.4929d2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7402e6d107b21648db8e",
+    "url": "/elihpoeht-react/static/js/main.25ac246f.chunk.js"
+  },
+  {
+    "revision": "f4262096917ea5ee41fb",
+    "url": "/elihpoeht-react/static/js/runtime-main.3b1b0cdf.js"
+  }
+]);
